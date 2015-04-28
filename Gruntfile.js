6a9a84b7795433564c0635790a0cc3b5d95a488b
vicchi <gary@vicchi.org>
2015-04-28 13:39:23 +0100

    First pass of rework; use Grunt to build CSS and JS source files (yes, the SCSS is just a clone of app.css for now)

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js: {
+                src: [
+                    // 'bower_components/jquery/dist/jquery.js',
+                    // 'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+                    'src/js/bootleaf.js'
+                ],
+                dest: 'assets/js/bootleaf.js'
+            }
+        },
+        jshint: {
+            beforeconcat: ['src/js/bootleaf.js']
+        },
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: 'none'
+                },
+                files: [
+                    {
+                        'assets/css/bootleaf.css' : 'src/sass/bootleaf.scss'
+                    }
+                ]
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            grunt: {
+                files: ['Gruntfile.js'],
+                tasks: ['build']
+            },
+            sass: {
+                files: ['src/sass/*.scss'],
+                tasks: ['sass']
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: ['jshint', 'concat']
+            }
+        }
+    });
+
+    grunt.registerTask('default', 'watch');
+    grunt.registerTask('nodsstore', function() {
+        grunt.file.expand({
+            filter: 'isFile',
+            cwd: '.'
+        }, ['**/.DS_Store']).forEach(function(file) {
+            grunt.file.delete(file);
+        });
+    });
+    grunt.registerTask('warn-fail', 'Fail on warning log', function() {
+        var log = grunt.log;
+        var _warn = log.warn;
+        log.warn = function() {
+            _warn.apply(log, arguments);
+            grunt.fail.warn('Warning triggered; failing horribly');
+        }
+    });
+    grunt.registerTask('build', ['nodsstore', 'warn-fail', 'sass', 'jshint', 'concat']);
+}
